13b0105e334f12a002cfe9922e262af2f2c77eb0
Zan-afk <aniket.zanje01@gmail.com>
2024-03-06 23:29:45 +0530

    again commiting

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(5)
+
+
+    const addValue =() => {
+      //counter=counter+1;
+      if(counter < 10){
+        setCounter(counter+1)
+        console.log(counter, "value added")
+      }
+    }
+
+    
+    const removeValue =() => {
+
+      if(counter > 0){
+        setCounter(counter-1)
+      }
+    }
+  
+
+  return (
+    <>
+      <h1>counter project</h1>
+      <h3>counter value : {counter}</h3>
+
+      <button onClick={addValue}>Increase Value {counter}</button>
+      <br/> <br/>
+      <button onClick={removeValue}>Decrease Value {counter}</button>
+      <p>footer: {counter}</p>
+
+    </>
+  )
+}
+
+export default App
